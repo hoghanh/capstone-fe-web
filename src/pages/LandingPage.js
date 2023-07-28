79554d0c7e1573df548c1423cbae3a91852a97ee
hoghanh <hanhchse150754@fpt.edu.vn>
2023-07-28 15:39:15 +0700

    change header

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,21 +1,21 @@
-import React from "react";
-import Header from "../layout/header/Header";
-import Footer from "../layout/footer/Footer";
-import Banner1 from "../components/banner/Banner1";
-import Banner2 from "../components/banner/Banner2";
-import Banner3 from "../components/banner/Banner3";
-import JobPopular from "../components/carousel/JobPopular";
-import { Layout } from "antd";
+import React from 'react';
+import Header from '../layout/header/Header';
+import Footer from '../layout/footer/Footer';
+import Banner1 from '../components/banner/Banner1';
+import Banner2 from '../components/banner/Banner2';
+import Banner3 from '../components/banner/Banner3';
+import JobPopular from '../components/carousel/JobPopular';
+import { Layout } from 'antd';
 
 const LandingPage = () => {
   return (
     <>
+      ssss
       <Header />
       <Banner1 />
-      <Layout.Content style={{ maxWidth: 1080, margin: "0 auto" }}>
+      <Layout.Content style={{ maxWidth: 1080, margin: '0 auto' }}>
         <JobPopular />
       </Layout.Content>
-
       <Banner2 />
       <Banner3 />
       <Footer />
