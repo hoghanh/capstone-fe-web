1efd5fd84803916ed1edf199bc407823dcd6635c
hoghanh <hanhchse150754@fpt.edu.vn>
2023-08-02 22:05:23 +0700

    [Hanh] - layout: add marginTop Footer

--- src/styles/footer.js
+++ src/styles/footer.js
@@ -1,32 +1,33 @@
-import color from "./color";
-import theme from "./theme";
+import color from './color';
+import theme from './theme';
 
 export const footer = {
   backgroundColor: color.colorDeepSeaBlue,
-  padding: "30px 0",
+  marginTop: 80,
+  padding: '30px 0',
   text: {
-    color: "#fff",
+    color: '#fff',
     fontWeight: 700,
   },
   firstrow: {
     ...theme.responseWidth,
-    padding: "20px 30px",
+    padding: '20px 30px',
   },
   secondrow: {
     ...theme.responseWidth,
     padding: 10,
-    justifyContent: "space-between",
-    borderBottom: "1px solid #EDF6FF",
+    justifyContent: 'space-between',
+    borderBottom: '1px solid #EDF6FF',
   },
-  containIcon: { display: "flex", justifyContent: "center", gap: 10 },
-  icons: { fontSize: 25, color: "#fff" },
+  containIcon: { display: 'flex', justifyContent: 'center', gap: 10 },
+  icons: { fontSize: 25, color: '#fff' },
   name: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
     height: 50,
     text: {
-      color: "#fff",
+      color: '#fff',
     },
   },
 };
