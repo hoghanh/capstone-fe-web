22ba845399be4e11435ae785b0a04b939b1a9915
hoghanh <hanhchse150754@fpt.edu.vn>
2023-08-31 17:44:14 +0700

    [Hanh] change config

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const config = {
-  APP_API_URL: "https://fpt-sep.onrender.com",
-  LOCALSTORAGE_TOKEN_NAME: "token",
+  FPTSEP: 'https://fpt-sep.onrender.com',
+  LOCALSTORAGE_TOKEN_NAME: 'token',
 };
 
 export const { FPTSEP, LOCALSTORAGE_TOKEN_NAME } = config;
