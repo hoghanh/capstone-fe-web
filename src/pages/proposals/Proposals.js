c55fa19c88d5fc53bcfb6bb38a92fb2550d1b575
Nguyễn Hữu Phạm Tuân <tuanpham.25201@gmail.com>
2023-10-02 07:45:11 +0700

    Tuan - proposals - search

--- src/pages/proposals/Proposals.js
+++ src/pages/proposals/Proposals.js
@@ -1,11 +1,30 @@
 import {  Layout } from 'antd'
-import React from 'react'
+import React, { useEffect } from 'react'
 import ProposalsTracking from './ProposalsTracking'
+import { useRecoilState } from 'recoil';
+import { get } from 'utils/APICaller';
+import { proposalListState } from 'recoil/atom';
 
 
 
 
 const Proposals = () => {
+  const [proposals, setProposals] = useRecoilState(proposalListState);
+
+  useEffect(() => {
+    getProposals();
+  }, []);
+  const getProposals = async () => {
+    await get({ endpoint: `/proposal/freelancer/1` })
+      .then((response) => {
+        const data = response.data;
+        setProposals(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <>
     <Layout.Content className={'containerBody'} style={styles.containerBody}>
