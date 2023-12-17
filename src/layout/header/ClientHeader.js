ccf1915981b7d55139d1c43273425ce72757d8d2
hoghanh <hanhchse150754@fpt.edu.vn>
2023-12-17 15:56:06 +0700

    Overflow notification

--- src/layout/header/ClientHeader.js
+++ src/layout/header/ClientHeader.js
@@ -234,11 +234,8 @@ function ClientHeader({ name, subName, onPress }) {
                     zIndex: 999,
                     border: '1px solid #f5f5f5',
                     borderRadius: '10px',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    gap: '2px',
                     background: '#F7F8F9',
-                    overflow: 'scroll',
+                    overflowY: 'scroll',
                     boxShadow:
                       'rgba(0, 0, 0, 0.03) 4px 5px 6px 4px, rgba(0, 0, 0, 0.02) 4px 5px 10px 3px, rgba(0, 0, 0, 0.02) 4px 6px 8px 4px',
                   }}
